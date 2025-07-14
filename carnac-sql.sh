326dc4a227168de34b7b50a0436677fe8f7a4e67
dodderingstalwart <git@github.com:dodderingstalwart/strabler.git>
2025-07-13 17:41:19 -0700

    sql script

---
+++ carnac-sql.sh
@@ -0,0 +1,11 @@
+CREATE DATABASE carnac;
+USE carnac;
+CREATE TABLE Insults (ID int NOT NULL AUTO_INCREMENTS, 
+                      Insult varchar(255) NOT NULL,
+                      PRIMARY KEY (ID) 
+);
+CREATE TABLE Jokes (ID int NOT NULL AUTO_INCREMENTS,
+                    Answer varchar(255) NOT NULL,
+                    Question varchar(255) NOT NULL,
+                    PRIMARY KEY (ID)
+);
