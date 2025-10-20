5fa97a19fe70e1fd07e2b5af9705b1757f744de5
dodderingstalwart <106283860+dodderingstalwart@users.noreply.github.com>
2025-10-20 22:07:12 +0000

    Spelling error for init.sql

--- init.sql
+++ init.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS Insults; 
-CREATE TABLE Insults (ID int NOT NULL AUTO_INCREMENTS, 
+CREATE TABLE Insults (ID int NOT NULL AUTO_INCREMENT, 
                       Insult varchar(255) NOT NULL DISTINCT,
                       PRIMARY KEY (ID) 
 );
-CREATE TABLE Jokes (ID int NOT NULL AUTO_INCREMENTS,
+CREATE TABLE Jokes (ID int NOT NULL AUTO_INCREMENT,
                     Answer varchar(255) NOT NULL,
                     Question varchar(255) NOT NULL,
                     PRIMARY KEY (ID)
