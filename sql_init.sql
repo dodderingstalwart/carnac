48b60cb7aa22812e4eac7cc2820f481fcfb73a2f
dodderingstalwart <git@github.com:dodderingstalwart/strabler.git>
2025-12-22 21:06:34 +0000

    sql init file for mysql/mariadb

--- sql_init.sql
+++ sql_init.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS Insults;
-<<<<<<< HEAD
 CREATE TABLE Insults (ID int NOT NULL AUTO_INCREMENT,
                       insult varchar(255) NOT NULL,
                       PRIMARY KEY (ID)
@@ -10,8 +9,6 @@ CREATE TABLE Jokes (ID int NOT NULL AUTO_INCREMENT,
                     Question varchar(255) NOT NULL,
                     PRIMARY KEY (ID)
 );
-=======
 CREATE TABLE Insults ();
 DROP TABLE IF EXISTS Jokes;
 CREATE TABLE Jokes ();
->>>>>>> b1dd68f241d4dca11f7c83d126ca14a9257ecbbb
